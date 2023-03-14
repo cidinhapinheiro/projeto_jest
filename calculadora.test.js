a3a25ff272a685d2d93496e3a0ec8fe00a465b38
cidinhapinheiro <cidinha.posto@gmail.com>
2023-03-14 19:19:26 -0300

    testando

--- calculadora.test.js
+++ calculadora.test.js
@@ -8,10 +8,10 @@ test('subtracao de 2 números:', function() {
     expect(calculadora.subtracao(6,3)).toBe(3)
 })
 
-test('multiplicacao de 2 números :', function() {
+test('multiplicacao de 2 números :', () => {
     expect(calculadora.multiplicacao(2,3)).toBe(6)
 })
 
-test('divisao de 2 números:', function() {
+test('divisao de 2 números:',() => {
     expect(calculadora.divisao(6,3)).toBe(2)
 })
